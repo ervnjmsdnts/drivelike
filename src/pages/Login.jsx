f907db2ffbb7bd85ad0073989f114f2f349dbf68
ervnjmsdnts <earvinjamesdantes@gmail.com>
2023-05-22 00:07:12 +0800

    update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Link, Stack, Typography } from '@mui/material';
+import { Box, Button, Container, Link, Stack, Typography } from '@mui/material';
 import { Link as RouterLink } from 'react-router-dom';
 import Input from '../components/Input';
 import { useMutation } from 'react-query';
@@ -9,6 +9,25 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { toast } from 'react-toastify';
 import { handleKeyDown } from '../helpers';
 import MainLogin from '/mainlogin.jpg';
+
+const trivias = [
+  {
+    desc: 'The word “hundred” comes from the old Norse term, “hundrath”, which actually means 120 and not 100.',
+    image: '/trivia1.jpg'
+  },
+  {
+    desc: '“Forty” is the only number that is spelt with letters arranged in alphabetical order.',
+    image: '/trivia2.jpg'
+  },
+  {
+    desc: 'Four’ is the only number in the English language that is spelt with the same number of letters as the number itself.',
+    image: '/trivia3.jpg'
+  },
+  {
+    desc: 'From 0 to 1000, the only number that has the letter “a” in it is “one thousand”.',
+    image: '/trivia4.jpg'
+  }
+];
 
 const loginSchema = z.object({
   username: z
@@ -49,90 +68,131 @@ const Login = () => {
   };
 
   return (
-    <Box
-      sx={{ display: 'flex', height: '100vh' }}
-      onKeyDown={(e) => handleKeyDown(e, handleSubmit(onSubmit))}
-    >
+    <Box>
       <Box
-        sx={{
-          display: 'flex',
-          width: '40%',
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
+        sx={{ display: 'flex', height: '95vh' }}
+        onKeyDown={(e) => handleKeyDown(e, handleSubmit(onSubmit))}
       >
         <Box
           sx={{
             display: 'flex',
-            alignItems: 'center',
-            width: '100%',
-            height: '100%',
-            marginX: '16px',
-            borderRadius: '16px',
-            padding: '16px'
+            width: '40%',
+            justifyContent: 'center',
+            alignItems: 'center'
           }}
         >
-          <Stack gap="8px" width="100%">
-            <Typography
-              fontWeight="bold"
-              textAlign="center"
-              mb="16px"
-              variant="h4"
-            >
-              Welcome to Math e-Turo
-            </Typography>
-            <Box
-              display="flex"
-              justifyContent="center"
-              alignItems="center"
-              flexDirection="column"
-              gap="16px"
-            >
-              <Input
-                label="Email"
-                errors={errors.username}
-                {...register('username', { required: true })}
-              />
-              <Input
-                label="Password"
-                type="password"
-                errors={errors.password}
-                {...register('password')}
-              />
-              <Button
-                variant="contained"
-                fullWidth
-                size="large"
-                onClick={handleSubmit(onSubmit)}
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              width: '100%',
+              height: '100%',
+              marginX: '16px',
+              borderRadius: '16px',
+              padding: '16px'
+            }}
+          >
+            <Stack gap="8px" width="100%">
+              <Typography
+                fontWeight="bold"
+                textAlign="center"
+                mb="16px"
+                variant="h4"
               >
-                Login
-              </Button>
-            </Box>
-            <Box sx={{ display: 'flex', justifyContent: 'end' }}>
-              <Link
-                component={RouterLink}
-                underline="none"
-                to="forgot-password"
+                Welcome to Math e-Turo
+              </Typography>
+              <Box
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                flexDirection="column"
+                gap="16px"
               >
-                Forgot your password?
-              </Link>
-            </Box>
-          </Stack>
+                <Input
+                  label="Email"
+                  errors={errors.username}
+                  {...register('username', { required: true })}
+                />
+                <Input
+                  label="Password"
+                  type="password"
+                  errors={errors.password}
+                  {...register('password')}
+                />
+                <Button
+                  variant="contained"
+                  fullWidth
+                  size="large"
+                  onClick={handleSubmit(onSubmit)}
+                >
+                  Login
+                </Button>
+              </Box>
+              <Box sx={{ display: 'flex', justifyContent: 'end' }}>
+                <Link
+                  component={RouterLink}
+                  underline="none"
+                  to="forgot-password"
+                >
+                  Forgot your password?
+                </Link>
+              </Box>
+            </Stack>
+          </Box>
+        </Box>
+        <Box
+          display="flex"
+          width="100%"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Box
+            component="img"
+            src={MainLogin}
+            alt="MainLogin"
+            sx={{ width: 1000 }}
+          />
         </Box>
       </Box>
-      <Box
-        display="flex"
-        width="100%"
-        justifyContent="center"
-        alignItems="center"
-      >
-        <Box
-          component="img"
-          src={MainLogin}
-          alt="MainLogin"
-          sx={{ width: 1000 }}
-        />
-      </Box>
+      <Container sx={{ pb: 2 }}>
+        {trivias.map((trivia, index) => (
+          <Box
+            key={index}
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              mb: 2
+            }}
+          >
+            {index % 2 === 0 ? (
+              <>
+                <Typography maxWidth="500px" variant="h6">
+                  {trivia.desc}
+                </Typography>
+                <Box
+                  component="img"
+                  sx={{ borderRadius: 2, width: '400px' }}
+                  src={trivia.image}
+                  alt={trivia.image}
+                />
+              </>
+            ) : (
+              <>
+                <Box
+                  component="img"
+                  sx={{ borderRadius: 2, width: '400px' }}
+                  src={trivia.image}
+                  alt={trivia.image}
+                />
+                <Typography maxWidth="500px" variant="h6">
+                  {trivia.desc}
+                </Typography>
+              </>
+            )}
+          </Box>
+        ))}
+      </Container>
     </Box>
   );
 };
